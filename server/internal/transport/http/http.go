b045620c6a589a56694aaef43406ba174106d087
decadaze <nikitakovalenko7219@gmail.com>
2026-04-16 17:32:28 +0500

    started routes

--- server/internal/transport/http/http.go
+++ server/internal/transport/http/http.go
@@ -3,16 +3,27 @@ package http
 import (
 	"ural-hackaton/internal/services"
 	"ural-hackaton/internal/transport/http/controllers"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type Http struct {
 	services    *services.Services
 	controllers *controllers.Controllers
+	router      *fiber.App
 }
 
-func Init(services *services.Services) *Http {
+func Init(services *services.Services, router *fiber.App) *Http {
 	return &Http{
 		services:    services,
 		controllers: controllers.Init(services),
+		router:      router,
 	}
+}
+
+func (h *Http) Start() {
+	h.controllers.AdminController.RegisterRoutes(h.router)
+	h.controllers.MentorController.RegisterRoutes(h.router)
+	h.controllers.UserController.RegisterRoutes(h.router)
+	h.controllers.RequestController.RegisterRoutes(h.router)
 }
