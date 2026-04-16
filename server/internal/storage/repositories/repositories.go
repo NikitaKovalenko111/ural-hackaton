a25482ffb27f74a8fea36a14b2b377b2843882ce
IDK <IDK789@yandex.ru>
2026-04-16 14:40:28 +0500

    add init repos, add init services, add func init services

--- server/internal/storage/repositories/repositories.go
+++ server/internal/storage/repositories/repositories.go
@@ -2,19 +2,21 @@ package repositories
 
 import (
 	"ural-hackaton/internal/storage"
+	admin_storage "ural-hackaton/internal/storage/repositories/admins"
+	hub_storage "ural-hackaton/internal/storage/repositories/hubs"
 	user_storage "ural-hackaton/internal/storage/repositories/users"
 )
 
 type Repositories struct {
-	UserRepository *user_storage.UserRepo
+	UserRepository  *user_storage.UserRepo
+	HubRepository   *hub_storage.HubRepo
+	AdminRepository *admin_storage.AdminRepo
 }
 
 func InitRepositories(db *storage.Storage) *Repositories {
-	userRepository := user_storage.Init(db)
-
-	Repositories := Repositories{
-		UserRepository: userRepository,
+	return &Repositories{
+		UserRepository:  user_storage.Init(db),
+		HubRepository:   hub_storage.Init(db),
+		AdminRepository: admin_storage.Init(db),
 	}
-
-	return &Repositories
 }
