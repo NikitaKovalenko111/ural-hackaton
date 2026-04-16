3474c0895553e5325b3896085709d527888e5776
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2026-04-17 03:17:21 +0500

    swagger

--- server/internal/transport/http/http.go
+++ server/internal/transport/http/http.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"ural-hackaton/internal/docs"
 	"ural-hackaton/internal/services"
 	"ural-hackaton/internal/transport/http/controllers"
 
@@ -22,6 +23,21 @@ func Init(services *services.Services, router *fiber.App) *Http {
 }
 
 func (h *Http) Start() {
+	h.router.Get("/swagger", func(ctx *fiber.Ctx) error {
+		ctx.Type("html", "utf-8")
+		return ctx.SendString(docs.SwaggerHTML)
+	})
+
+	h.router.Get("/swagger/openapi.yaml", func(ctx *fiber.Ctx) error {
+		data, err := docs.OpenAPISpec.ReadFile("openapi.yaml")
+		if err != nil {
+			return err
+		}
+
+		ctx.Type("yaml", "utf-8")
+		return ctx.Send(data)
+	})
+
 	h.controllers.AdminController.RegisterRoutes(h.router)
 	h.controllers.MentorController.RegisterRoutes(h.router)
 	h.controllers.UserController.RegisterRoutes(h.router)
